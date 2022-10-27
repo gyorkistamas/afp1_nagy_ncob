5e8fa5007b087e93afc9d3394e7db166e9bac667
unknown <karacsonybalazs01@gmail.com>
2022-10-27 09:12:28 +0200

    UserController osztály létrehozása

--- picquiz/app/Http/Controllers/UserController.php
+++ picquiz/app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    // Show Register/Create Form
+    // Show Register/Create Form:
     public function create() {
         return view('users.register');
     }
