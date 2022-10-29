574148fedb133ad41b7b226d07812f4612b84e65
Tamás Györkis <tamasgyorkis@gmail.com>
2022-10-29 12:07:33 +0200

    főoldal - háttér, betűtípus, alapváz hozzáadása

--- picquiz/resources/views/mainpage.blade.php
+++ picquiz/resources/views/mainpage.blade.php
@@ -7,4 +7,29 @@
 
 @section('content')
 
+<div class="container bg-dark text-white p-4 rounded-3" style="--bs-bg-opacity: .98;">
+
+	<div class="row">
+		<div class="col-12 text-center">
+			<h1>Üdv a PicQuiz oldalon!</h1>
+		</div>
+	</div>
+
+	<hr>
+
+
+	<div class="row">
+
+		<div class="col-12">
+
+		</div>
+
+		<div class="col-12">
+			<h3>A játék lényege:</h3>
+			<p>A játék során képeket fogsz látni játékokból kiemelve. A feladatot, hogy meghatározd a játék címét.</p>
+		</div>
+	</div>
+
+</div>
+
 @endsection
