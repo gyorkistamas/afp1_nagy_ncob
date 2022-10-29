c8164ae298c05a18a8a3400c45c3c6a8eea3d16c
Tamás Györkis <tamasgyorkis@gmail.com>
2022-10-29 12:29:10 +0200

    főoldal - képek hozzáadása

--- picquiz/resources/views/mainpage.blade.php
+++ picquiz/resources/views/mainpage.blade.php
@@ -17,16 +17,44 @@
 
 	<hr>
 
-
 	<div class="row">
 
-		<div class="col-12">
-
+		<div class="col-12 d-flex justify-content-center">
+			<div id="carouselExampleIndicators" class="carousel slide w-50" data-bs-ride="true">
+				<div class="carousel-indicators">
+				  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+				  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+				  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				</div>
+				<div class="carousel-inner">
+				  <div class="carousel-item active">
+					<img src="images/samplePictures/pacman.jpg" class="d-block w-100" alt="Pac-man">
+				  </div>
+				  <div class="carousel-item">
+					<img src="images/samplePictures/tetris.jpg" class="d-block w-100" alt="Tetris">
+				  </div>
+				  <div class="carousel-item">
+					<img src="images/samplePictures/cod.jpg" class="d-block w-100" alt="Call of Duty">
+				  </div>
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+				  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				  <span class="visually-hidden">Előző</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+				  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				  <span class="visually-hidden">Következő</span>
+				</button>
+			  </div>
 		</div>
+
+		<hr class="mt-2">
 
 		<div class="col-12">
 			<h3>A játék lényege:</h3>
-			<p>A játék során képeket fogsz látni játékokból kiemelve. A feladatot, hogy meghatározd a játék címét.</p>
+			<p>A játék során képeket fogsz látni játékokból kiemelve. A feladatod, hogy meghatározd a játék címét. A végén kiderül, hogy mennyit találtál el. Ha szeretnéd elmenteni az eredményed, akkor regisztrálj az oldalon!</p>
+			<h6>Jó szorakozást kívánunk!</h6>
+			<a class="btn btn-success" href="#">Új játék indítása</a>
 		</div>
 	</div>
 
