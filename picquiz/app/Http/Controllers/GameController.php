3c53af783ff5644982b8597d7a5e75eee2275223
anon <anon@anon.anon>
2022-11-03 07:38:17 +0100

    + game controller

---
+++ picquiz/app/Http/Controllers/GameController.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Puzzle;
+use App\Models\Game;
+use App\Models\GamePuzzle;
+ 
+class GameController extends Controller
+{
+    public function generate(Request $request)
+    {
+		Game::factory()->len((int)$request->post('game_len'))->create();
+		return redirect('/play');
+    }
+}
+
+?>
