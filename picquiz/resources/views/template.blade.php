4dd9436bba76bc7102720629a8b2c16dd66c3273
Tamás Györkis <tamasgyorkis@gmail.com>
2022-11-03 08:15:39 +0100

    menü - jelenlegi oldal kijelölés új játék esetén

--- picquiz/resources/views/template.blade.php
+++ picquiz/resources/views/template.blade.php
@@ -37,7 +37,7 @@
 					</li>
 
 					<li class="nav-item mt-2 mt-lg-0">
-						<a href="/new" class="nav-link navitem mt-2 mt-xl-0"><img
+						<a href="/new" class="nav-link navitem mt-2 mt-xl-0 {{ Request::is('new') ? 'active' : '' }}"><img
 								src="/images/icons/newGameIcon.png">Új játék</a>
 					</li>
 
