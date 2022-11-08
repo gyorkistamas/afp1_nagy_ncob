2604f0bd0e6f86b6d1b6864893da7855f502d2e6
anon <anon@anon.anon>
2022-11-08 18:07:03 +0100

    results - better layout

--- picquiz/resources/views/game/results.blade.php
+++ picquiz/resources/views/game/results.blade.php
@@ -8,26 +8,33 @@
 <!--{{ var_dump($puzzles); }}-->
 <div class="container bg-dark text-white p-4 rounded-3" style="--bs-bg-opacity: .98;">
 	<h1>Eredmények:</h1>
-		@php
-			$i = 1;
-			foreach($puzzles as $p):
-		@endphp
-			<div class="col-3 d-inline-block
-						border border-6 rounded
-						{{ $p->hit ? "border-success bg-success" : "border-danger bg-danger" }}
-						p-2
-						"
-			>
-				<h6>{{ $i }}&#41; {{ ucwords($p->answer) }}</h6>
-				<img class="col-12 border border-2 border-dark"
-						src="{{ $p->picture }}"
-						onerror="this.onerror=null; this.src='/images/undefined.jpg'"
-				>
-				</img>
+	<hr class="p-4">
+		<div class="container">
+			<div class="row g-4">
+				@php
+					$i = 1;
+					foreach($puzzles as $p):
+				@endphp
+					<div class="col m-1">
+						<div class="
+									border border-6 rounded
+									{{ $p->hit ? "border-success bg-success" : "border-danger bg-danger" }}
+									p-2
+								"
+						>
+							<h6>{{ $i }}&#41; {{ ucwords($p->answer) }}</h6>
+							<img class="col-12 border border-2 border-dark"
+									src="{{ $p->picture }}"
+									onerror="this.onerror=null; this.src='/images/undefined.jpg'"
+							>
+							</img>
+						</div>
+					</div>
+				@php
+					$i++;
+					endforeach;
+				@endphp
 			</div>
-		@php
-			$i++;
-			endforeach;
-		@endphp
+		</div>
 </div>
 @endsection
