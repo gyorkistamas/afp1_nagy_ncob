9d5cf32ff13c4feabc84f3686fae7b67284f95a3
Tamás Györkis <tamasgyorkis@gmail.com>
2022-11-08 10:24:56 +0100

    jelszó szövegek magyarítása

--- picquiz/lang/hu/passwords.php
+++ picquiz/lang/hu/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Jelszava vissza lett állítva!',
+    'sent' => 'E-mailben elküldtük jelszavát!',
+    'throttled' => 'Kérjük várjon mielőtt újra próbálkozik.',
+    'token' => 'Ez a jelszó visszaállítási token érvénytelen.',
+    'user' => "Nem találtunk ilyen e-mail címmel rendelkező felhasználót.",
 
 ];
