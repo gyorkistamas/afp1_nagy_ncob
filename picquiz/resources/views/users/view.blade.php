5fd2ebcb55a5fa1d4f904d4d036dc565d76ce94d
unknown <karacsonybalazs01@gmail.com>
2022-11-08 20:55:01 +0100

    Felhasználó adatainak módosítása

--- picquiz/resources/views/users/view.blade.php
+++ picquiz/resources/views/users/view.blade.php
@@ -1,7 +1,8 @@
 @extends('template')
 
 @section('title')
-<title>{{ Str::title(Auth::User()->username) }} megtekintése</title>
+{{-- {{ Str::title($User->username) }} --}}
+<title>Felhasználó megtekintése</title>
 @endsection
 
 @section('content')
@@ -14,31 +15,35 @@
         <div class="card bg-dark text-white" style="border-radius: 15px;">
           <div class="card-body text-center">
             <div class="mt-3 mb-4">
-              <img src="{{ asset(Auth::User()->profile_picture) }}"
+              <img src="{{ asset($User->profile_picture) }}"
                 class="rounded-circle img-fluid" style="width: 100px;" />
             </div>
-            <h4 class="mb-2">{{Auth::User()->username}}</h4>
-            <p class="text-white mb-4">{{Auth::User()->isAdmin == 1 ? '@Adminisztrátor' : '@Felhasználó'}} <span class="mx-2">|</span>
-                <a href="#!">{{Auth::User()->email}}</a>
+            <h4 class="mb-2">{{ $User->username }}</h4>
+            <p class="text-white mb-4">{{ $User->isAdmin == 1 ? '@Adminisztrátor' : '@Felhasználó' }} <span class="mx-2">|</span>
+                <a href="#!">{{ $User->email }}</a>
             </p>
 
             <div class="mb-4 pb-2">
-                <p class="mb-2 h5">{{Auth::User()->created_at}}</p>
+                <p class="mb-2 h5">{{ $User->created_at }}</p>
                 <p class="text-white mb-4">Csatlakozás ideje</p>
             </div>
 
+            {!! $User->id == Auth::User()->id ? "<a class='btn btn-success btn-rounded btn-lg' href='/users/edit'>Profil szerkesztése</a>" : "" !!}
+            {{-- @if ($User->id == Auth::User()->id)
             <a class="btn btn-success btn-rounded btn-lg" href="/users/edit">Profil szerkesztése</a>
+            @endif --}}
+
             <div class="d-flex justify-content-between text-center mt-5 mb-2">
               <div>
-                <p class="mb-2 h5">{{Auth::User()->numberOfGames}}</p>
+                <p class="mb-2 h5">{{ $User->numberOfGames }}</p>
                 <p class="text-white mb-0">Játszmák száma</p>
               </div>
               <div class="px-3">
-                <p class="mb-2 h5">{{Auth::User()->numberOfHits}}</p>
+                <p class="mb-2 h5">{{ $User->numberOfHits }}</p>
                 <p class="text-white mb-0">Eltalált feladványok</p>
               </div>
               <div>
-                <p class="mb-2 h5">{{ Auth::User()->numberOfGames == 0 ? 0 : (Auth::User()->numberOfHits / Auth::User()->numberOfGames) * 100 }} %</p>
+                <p class="mb-2 h5">{{ $User->numberOfGames == 0 ? 0 : ($User->numberOfHits / $User->numberOfGames) * 100 }} %</p>
                 <p class="text-white mb-0">Találati arány</p>
               </div>
             </div>
