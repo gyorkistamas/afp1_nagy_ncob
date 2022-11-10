b8cf45be80fd84b79a716f0c0bb5fc1db5d352fe
unknown <karacsonybalazs01@gmail.com>
2022-11-10 09:08:25 +0100

    Feladvány lista dizájnolása

--- picquiz/resources/views/game/puzzleList.blade.php
+++ picquiz/resources/views/game/puzzleList.blade.php
@@ -6,29 +6,36 @@
 
 @section('content')
 
-@if ( count($Puzzles) == 0 )
-    <h1>Hoppá! Még nincsenek feladványaink!</h1>
-@else
-    @foreach($Puzzles as $puzzle)
-        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-            <div class="card-header">Létrehozva: {{ $puzzle->created_at }}</div>
-            <img src={{ asset($puzzle->picture) }} class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Megfejtés: {{ $puzzle->answer }}</h5>
-              <p class="card-text">Feltöltötte: {{ $puzzle->username }}</p>
-              <p class="card-text">Találatok száma: {{ $puzzle->numberOfHits }}</p>
-              <p class="card-text">Játszmák száma: {{ $puzzle->numberOfGames }}</p>
-              <p class="card-text">Találati arány: {{ $puzzle->numberOfGames == 0 ? 0 : ($puzzle->numberOfHits / $puzzle->numberOfGames) * 100 }} %</p>
-            </div>
-          </div>
-    @endforeach
+<div class="container bg-dark text-white p-4 rounded-3 mb-4">
+    <div class="row justify-content-center gap-3">
+        @if ( count($Puzzles) == 0 )
+            <h1 class="text-center">Hoppá! Még nincsenek feladványaink!</h1>
+        @else
+            <h1 class="text-center">Feladványok:</h1>
+            @foreach($Puzzles as $puzzle)
+                <div class="card text-bg-dark mb-4 border border-5" style="max-width: 20rem;">
+                    <div class="card-header">
+                        <span class="badge bg-danger">Létrehozva: {{ $puzzle->created_at }}</span>
+                    </div>
+                    <img src={{ asset($puzzle->picture) }} class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title text-center">Megfejtés: {{ Str::upper($puzzle->answer) }}</h5>
+                    <p class="card-text font-weight-bold">Feltöltötte: {{ $puzzle->username }}</p>
+                    <p class="card-text font-weight-bold">Találatok száma: {{ $puzzle->numberOfHits }}</p>
+                    <p class="card-text font-weight-bold">Játszmák száma: {{ $puzzle->numberOfGames }}</p>
+                    <p class="card-text font-weight-bold">Találati arány: {{ $puzzle->numberOfGames == 0 ? 0 : ($puzzle->numberOfHits / $puzzle->numberOfGames) * 100 }} %</p>
+                    </div>
+                </div>
+            @endforeach
 
-    <div class="row">
-		<div class="col-12">
-			{{ $Puzzles->links() }}
-		</div>
-	</div>
-@endif
+            <div class="row">
+                <div class="col-12">
+                    {{ $Puzzles->links() }}
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
 
 
 
