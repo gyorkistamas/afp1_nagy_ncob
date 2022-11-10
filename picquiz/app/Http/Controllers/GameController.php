8b02a698d56cb024cfe2c1bc4f610905b1a1c605
anon <anon@anon.anon>
2022-11-10 09:00:23 +0100

    game - string as puzzle number error handling

--- picquiz/app/Http/Controllers/GameController.php
+++ picquiz/app/Http/Controllers/GameController.php
@@ -37,6 +37,10 @@ class GameController extends Controller
     }
 
 	public function play($id, $puzzle){
+		$puzzle = (int)$puzzle;
+		if($puzzle == 0){
+			abort(404);
+		}
 		$p = DB::table('game_puzzles')->join('puzzles', 'puzzle_id', '=', 'puzzles.id')->get()->where('game_id', $id)->values()->get($puzzle-1);
 		if($p == NULL){
 			return abort(404);
