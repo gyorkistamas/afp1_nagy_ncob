60f6b021e3077c268e349f8ff5d0ca1ad9cae88a
Tamás Györkis <tamasgyorkis@gmail.com>
2022-11-15 12:13:49 +0100

    adatbázis hiba elkapás javítása

--- picquiz/resources/views/errors/database.blade.php
+++ picquiz/resources/views/errors/database.blade.php
@@ -1,14 +1,26 @@
-@extends('template')
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-@section('title')
-<title>Hiba az adatbázis kapcsolat létrehozásánál!</title>
-@endsection
+	<link rel="icon" href="/favicon.ico">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="/js/navbar.js" defer></script>
 
-@section('content')
+	<link rel="stylesheet" type="text/css" href="/css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="/css/template.css">
 
-<div class="container bg-dark text-white p-4 rounded-3 text-center" style="--bs-bg-opacity: .98;">
+    <title>Hiba!</title>
+</head>
+<body>
+
+<div class="container bg-dark text-white p-4 rounded-3 text-center mt-5" style="--bs-bg-opacity: .98;">
 	<h1>Hiba történt az adatbázissal való kommunikáció során!</h1>
 	<button class="btn btn-danger" onClick="window.location.reload();">Oldal újratöltése</button>
 </div>
-@endsection
+</body>
+</html>
