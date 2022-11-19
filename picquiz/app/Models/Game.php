44dcc92530745399035036a1e953f5adf8e30850
Tamás Györkis <tamasgyorkis@gmail.com>
2022-11-19 10:12:59 +0100

    Game mezők kitöltésének engedélyezése

--- picquiz/app/Models/Game.php
+++ picquiz/app/Models/Game.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'picture',
+        'answer',
+        'user_added'
+    ];
 }
